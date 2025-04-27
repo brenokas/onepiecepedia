d051740dfdb0fb1ed248e38590fa66e37bc84293
Breno de Freitas <brenofreitas28f@gmail.com>
2025-04-27 00:11:34 -0300

    Finalizando a pagina sobre os bandos

--- js/main.js
+++ js/main.js
@@ -11,9 +11,13 @@ const nomesSagas = [
 	'Zou',
 	'Whole Cake',
 	'Wano',
+	'Saga Final (em andamento)',
 ];
 
-const qtdEpisodios = [61, 74, 71, 119, 59, 132, 58, 54, 122, 32, 107, 199];
+const qtdEpisodios = [61, 74, 71, 119, 59, 132, 58, 54, 122, 32, 107, 199, 34];
+
+const nomesCapitaes = ['Monkey D. Luffy', 'Shanks, "O ruivo"', 'Marshall D. Teach', 'Kaido', 'Big Mom', 'Buggy', 'Barba Branca'];
+const recompensasCapitaes = [3000000000, 4048900000, 3996000000,4611100000,  4388000000, 3189000000, 5046000000];
 
 document.addEventListener('DOMContentLoaded', () => {
 	new Chart(document.getElementById('graficoEpisodios'), {
@@ -39,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 				datalabels: {
 					align: 'top',
 					anchor: 'end',
-					color: '#FFF',
+					color: '#fefefe',
 					font: {
 						weight: 'bold',
 						size: 12,
@@ -86,17 +90,107 @@ document.addEventListener('DOMContentLoaded', () => {
 							weight: 'bold',
 						},
 					},
-                    ticks: {
-                        stepSize: 20,
-                        color: '#fefefe',
-                        font: {
-                            size: 12
-                        }
-                    },
-                    grid: {
+					ticks: {
+						stepSize: 20,
+						color: '#fefefe',
+						font: {
+							size: 12,
+						},
+					},
+					grid: {
 						color: 'rgb(254, 254, 254, 0.10)',
-					},  
+					},
+				},
+			},
+		},
 
+		plugins: [ChartDataLabels],
+	});
+
+	new Chart(document.getElementById('graficoRecompensas'), {
+		type: 'bar',
+		data: {
+			labels: nomesCapitaes,
+			datasets: [
+				{
+					label: '',
+					data: recompensasCapitaes,
+					borderWidth: 1,
+					backgroundColor: '#F8DE3C',
+					tension: 0.4,
+					
+				},
+			],
+		},
+		options: {
+			responsive: true,
+			plugins: {
+				legend: {
+					display: false,
+				},
+				datalabels: {
+					align: 'top',
+					anchor: 'end',
+					color: '#fefefe',
+					font: {
+						weight: 'bold',
+						size: 12,
+					},
+					formatter: function(value) {
+						return new Intl.NumberFormat('pt-BR', {minimumFractionDigits: 2}).format(value)
+					}
+				},
+			},
+
+			interaction: {
+				mode: 'index',
+			},
+
+			scales: {
+				x: {
+					title: {
+						display: true,
+						text: 'Capitães',
+						color: '#fefefe',
+						font: {
+							size: 15,
+							weight: 'bold',
+						},
+					},
+					ticks: {
+						color: '#fefefe',
+						font: {
+							size: 12,
+						},
+					},
+					grid: {
+						color: 'rgb(254, 254, 254, 0.10)',
+					},
+				},
+
+				y: {
+					beginAtZero: true,
+					min: 0,
+					max: 6000000000,
+					title: {
+						display: true,
+						text: 'Recompensa',
+						color: '#fefefe',
+						font: {
+							size: 15,
+							weight: 'bold',
+						},
+					},
+					ticks: {
+						stepSize: 1500000000,
+						color: '#fefefe',
+						font: {
+							size: 12,
+						}
+					},
+					grid: {
+						color: 'rgb(254, 254, 254, 0.10)',
+					},
 				},
 			},
 		},
