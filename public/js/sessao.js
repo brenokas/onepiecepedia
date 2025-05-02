3501fce7dbc6f1226e40d5d7341a2a7d92387b77
brenokas <brenofreitas28f@gmail.com>
2025-05-02 20:53:34 -0300

    Colocando o nome do usuário na tela de perfil

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,8 +1,12 @@
 function validarSessao() {
-	var email = sessionStorage.EMAIL_USUARIO;
-	var nome = sessionStorage.NOME_USUARIO;
+	let email = sessionStorage.EMAIL_USUARIO;
+	let nome = sessionStorage.NOME_USUARIO;
+
+	let nomeUsuario = document.getElementById('perfil-user-name');
 
 	if (email == null && nome == null) {
 		window.location = '../login.html';
+	} else {
+		nomeUsuario.innerHTML = `${nome}!`;
 	}
 }
