fed5fc1441ad45e1da3a6a662342bea3e9413908
Breno <brenofreitas28f@gmail.com>
2026-05-30 00:08:12 -0300

    refactor: mudando a disposição das pastas

---
+++ backend/src/main/java/com/brenokas/onepiecepedia/OnepiecepediaApplication.java
@@ -0,0 +1,13 @@
+package com.brenokas.onepiecepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepiecepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnepiecepediaApplication.class, args);
+	}
+
+}
